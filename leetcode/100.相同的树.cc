28ecd5d00fe21d3cd7db4ba9782551c8ae844f13
AimTao <net@aimtao.net>
2020-08-07 23:57:15 +0800

    Add #100

---
+++ leetcode/100.相同的树.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        } else if (p == nullptr || q == nullptr) {
+            return false;
+        }
+        if (p->val != q->val) {
+            return false;
+        }
+        if (!isSameTree(p->left, q->left)) { return false; }
+        if (!isSameTree(p->right, q->right)) { return false; }
+        return true;
+    }
+};
