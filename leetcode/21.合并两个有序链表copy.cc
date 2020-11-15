09e38ea72bc1604e767a34c29ae5447e4c77e333
AimTao <net@aimtao.net>
2020-11-15 21:03:30 +0800

    Add #21

---
+++ leetcode/21.合并两个有序链表copy.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *head=new ListNode(0); //这里的head不能赋值l1,l2，或者空值，只能赋值为一个别的地址
+        ListNode *p=head;
+        
+        while(l1!=NULL && l2!=NULL){
+            if(l1->val<=l2->val){
+                p->next=l1;
+                l1=l1->next;
+            }
+            else{
+                p->next=l2;
+                l2=l2->next;
+            }
+            p=p->next;
+        }
+        if(l1!=NULL)
+            p->next =l1;
+        if(l2!=NULL)
+            p->next =l2;
+        
+        return head->next;
+        
+        
+    }
+};
